9aeb0c8d0f35e88d533b14397cccfd914162f46e
dcordero10 <Codingpirate1!>
2021-08-09 14:58:06 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+MORSE_CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+        'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..',
+
+        '0': '-----',  '1': '.----',  '2': '..---',
+        '3': '...--',  '4': '....-',  '5': '.....',
+        '6': '-....',  '7': '--...',  '8': '---..',
+        '9': '----.'
+        }
+
+uncoverted_message = input("Please enter your message")
+message = [item for item in uncoverted_message]
+morse_message = []
+
+for letter in message:
+    if letter == " ":
+        morse_message.append("SPACE")
+        pass
+    for key, value in MORSE_CODE.items():
+        if key == letter.upper():
+            print(value)
+            morse_message.append(value)
+print(morse_message)
